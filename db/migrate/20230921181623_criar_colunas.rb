0aabfebc4c0af355530737485de962548f6c6c9b
Alex-dev-if <91799263+Alex-dev-if@users.noreply.github.com>
2023-09-29 00:26:55 -0300

    Delete db/migrate/20230921181623_criar_colunas.rb

--- db/migrate/20230921181623_criar_colunas.rb
+++
@@ -1,9 +0,0 @@
-class CriarColunas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :colunas do |t|
-      t.integer :todo
-      t.integer :doing
-      t.integer :done
-    end
-end
-end
