227039b1a9a3db986bbd8a98c681eb801ab69cb5
Alex-dev-if <alexnunessilva006@gmail.com>
2024-02-27 21:39:26 -0300

    deploy to render

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_181623) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_011015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "colunas", force: :cascade do |t|
-    t.integer "todo"
-    t.integer "doing"
-    t.integer "done"
-  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
